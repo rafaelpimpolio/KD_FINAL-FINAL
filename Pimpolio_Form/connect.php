86d65401df1f6583d06165a1b4d86e105ddbb902
Rafael <rafael.pimpolio@unc.edu.ph>
2026-01-04 15:46:38 +0800

    qwerttty

--- Pimpolio_Form/connect.php
+++ Pimpolio_Form/connect.php
@@ -1,20 +1,14 @@
 <?php
 class Database {
-    private static $dbName = "kd_sportswear";
-    private static $dbHost = "localhost";
-    private static $dbUser = "root";
-    private static $dbPass = "";
-    private static $conn = null;
-
     public static function Connection() {
-        if (self::$conn === null) {
-            self::$conn = new PDO(
-                "mysql:host=".self::$dbHost.";dbname=".self::$dbName,
-                self::$dbUser,
-                self::$dbPass,
-                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-            );
-        }
-        return self::$conn;
+        return new PDO(
+            "mysql:host=localhost;dbname=kd_sportswear;charset=utf8",
+            "root",
+            "",
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]
+        );
     }
 }
